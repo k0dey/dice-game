a4457cf89f405d546a8ef7345297caa605d7fa16
Kerry Parsons <kerryparsons1204@gmail.com>
2020-05-12 21:01:59 +0100

    added images and animation

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 
 const diceRoll = document.getElementById('rollDice');
 const diceBox = document.getElementById('diceBox');
-
+const page = document.getElementById('wrapper');
 diceRoll.onclick = rollDice;
-
 let currentRoll = 0;
 let rollTotal = 0;
 let maxTotal = 21;
 
 
 function rollDice () {
+    page.classList.add("spin");
+    setTimeout(function () {
+        page.classList.remove("spin");
+   },800);
+
     currentRoll = randomNum();
     rollTotal += currentRoll;
     if (currentRoll == 1) {
-        diceBox.innerHTML = `1! You lose!\ncurrent: ${currentRoll} - total: ${rollTotal}`;
+        diceBox.innerHTML = `<img src="./img/dice1.png"><br>1! You lose! Click roll to play again!<br><br>current: <b>${currentRoll}</b><br>total: <b>${rollTotal}</b>`;
     }
     else if (rollTotal >= 20) {
-        diceBox.innerHTML = `You win!\ncurrent: ${currentRoll} - total: ${rollTotal}`;
+        diceBox.innerHTML = `<img src="./img/dice${currentRoll}.png"><br>Winner! Click roll to play again!<br><br>current: <b>${currentRoll}</b><br>final total: <b>${rollTotal}</b>`;
         rollTotal = 0;
         } else {
             currentRoll = randomNum();
-            diceBox.innerHTML = `Keep going!\ncurrent: ${currentRoll} - total: ${rollTotal}`;
+            diceBox.innerHTML = `<img src="./img/dice${currentRoll}.png"><br>Keep going!<br><br>current: <b>${currentRoll}</b><br>total: <b>${rollTotal}</b>`;
     }
-
-
 
 
 }
 
-
 function randomNum () {
-    return Math.ceil((Math.random() * 6) + 1);
+    return Math.floor((Math.random() * 6) + 1);
 }

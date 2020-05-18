0f3418d12b23edc784a434bb9696e2fd9eed3faa
Kerry Parsons <kerryparsons1204@gmail.com>
2020-05-18 02:16:02 +0100

    added extra functionality to 2p game

--- script.js
+++ script.js
@@ -23,12 +23,19 @@ function selectMode() {
     pageTwo.classList.remove("hide");
     pageTwo.classList.add("unhide");
     onePlayer = false;
+    rollTotal = 0;
+    diceBox.innerHTML = `<img src="./img/dice1.png"><br><b>Click the button to roll!</b>`;
+
   } else {
     pageTwo.classList.remove("unhide");
     pageTwo.classList.add("hide");
     page.classList.remove("hide");
     page.classList.add("unhide");
     onePlayer = true;
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    diceBoxTwo.innerHTML = `<img src="./img/dice1.png"><br><b>Click the button to start a new game!</b>`;
+
   }
 }
 
@@ -38,7 +45,6 @@ let rollTotal = 0;
 function randomNum() {
   return Math.floor(Math.random() * 6 + 1);
 }
-
 
 function rollDice() {
   page.classList.add("spin");
@@ -60,8 +66,6 @@ function rollDice() {
   }
 }
 
-
-
 let playerOne = true;
 hold.onclick = () => {
   playerOne = !playerOne;
@@ -71,29 +75,24 @@ let playerOneScore = 0;
 let playerTwoScore = 0;
 
 function rollDiceTwo() {
-  //     pageTwo.classList.add("spin");
-  //     setTimeout(function () {
-  //         pageTwo.classList.remove("spin");
-  //    },600);
+  p1score.classList.remove("crown");
+  p2score.classList.remove("crown");
+
   currentRoll = randomNum();
 
-
   if (currentRoll == 1 && playerOne) {
-
     console.log(`p1 lose`);
     playerOneScore = 0;
     diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Too bad player 1! Player 2 you're up!</b><br><br>current: <b>${currentRoll}</b><br>current total: <b>${playerOneScore}</b>`;
     playerOne = false;
   } else if (playerOneScore + currentRoll >= 20) {
-
     playerOneScore += currentRoll;
-    diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Player 1 wins!</b>`;
-
+    diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Player 1 wins! Click roll to start a new game!</b>`;
+    p1score.classList.add("crown");
     console.log(`p1 win`);
     playerOneScore = 0;
     playerTwoScore = 0;
   } else if (playerOne) {
-
     console.log(`p1 roll`);
     playerOneScore += currentRoll;
     diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Keep going, Player 1!</b>`;
@@ -105,18 +104,18 @@ function rollDiceTwo() {
   } else if (playerTwoScore + currentRoll >= 20) {
     console.log(`p2 win`);
     playerTwoScore += currentRoll;
-    diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Player 2 wins!</b>`;
+    diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Player 2 wins! Click roll to start a new game!</b>`;
+    p2score.classList.add("crown");
     playerOneScore = 0;
     playerTwoScore = 0;
-  } else if (!playerOne){
+  } else if (!playerOne) {
     console.log(`p2 roll`);
     playerTwoScore += currentRoll;
     diceBoxTwo.innerHTML = `<img src="./img/dice${currentRoll}.png"><br><b>Keep going, Player 2!</b>`;
-
   } else {
-    alert(`error`)
+    alert(`error`);
   }
 
-  p1score.innerHTML = `<p><b>PLAYER 1</b><br>score: ${playerOneScore}</p>`
-  p2score.innerHTML = `<p><b>PLAYER 2</b><br>score: ${playerTwoScore}</p>`
+  p1score.innerHTML = `<p><b>PLAYER 1</b><br>score: ${playerOneScore}</p>`;
+  p2score.innerHTML = `<p><b>PLAYER 2</b><br>score: ${playerTwoScore}</p>`;
 }
